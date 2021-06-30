225d9fc9a5c00fbdc6e986c81bef1747cf781ecd
Zebych <81613417+Zebych@users.noreply.github.com>
2021-06-30 14:21:40 +0300

    accordion open/closed not button

--- my-app/src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ my-app/src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -2,27 +2,25 @@ import React, {useState} from "react";
 
 type AccordionTitlePropsType = {
     title: string
+    onClick:()=>void
 }
 type AccordionPropsType = {
     titleValue: string
 }
 
 export function UncontrolledAccordion(props: AccordionPropsType) {
-    const [toggle, setToggle] = useState(false)
-    let onClickHandler=()=>{
-        setToggle(!toggle)
-    }
+    const [collapsed, setCollapsed] = useState(false)
+
     return (<div>
-        <AccordionTitle title={props.titleValue}/>
-        {toggle && <AccordionBody/>}
-        <button onClick={onClickHandler}>TOGGLE</button>
+        <AccordionTitle title={props.titleValue} onClick={()=>{setCollapsed(!collapsed)}} />
+        {!collapsed && <AccordionBody/>}
     </div>)
 }
 
 
 export function AccordionTitle(props: AccordionTitlePropsType) {
     return (<div>
-        <h3>--{props.title}--</h3>
+        <h3 onClick={()=>{props.onClick()}}>--{props.title}--</h3>
     </div>)
 }
 

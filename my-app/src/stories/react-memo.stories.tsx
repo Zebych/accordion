bc34534efc82f8f1a1994a576a6ae73f918af336
Zebych <81613417+Zebych@users.noreply.github.com>
2021-07-20 16:17:15 +0300

    React.memo+useMemo

--- my-app/src/stories/react-memo.stories.tsx
+++ my-app/src/stories/react-memo.stories.tsx
@@ -1,26 +1,62 @@
-import React from "react"
-import { useState } from "react"
+import React, {useMemo} from "react"
+import {useState} from "react"
 
 export default {
-    title:'React.memo demo'
+    title: 'React.memo demo'
 }
-const NewMessageCounter=(props:{count:number})=>{
+const NewMessageCounter = (props: { count: number }) => {
     return <div>{props.count}</div>
 }
-const UsersSecret=(props:{users:Array<string>})=>{
+const UsersSecret = (props: { users: Array<string> }) => {
     return <div>
-        {props.users.map((u,i)=><div key={i}>{u}</div> )}
+        {props.users.map((u, i) => <div key={i}>{u}</div>)}
     </div>
 }
-const Users=React.memo(UsersSecret)
+const Users = React.memo(UsersSecret)
 
-export const Example=()=>{
-    const[counter,setCounter]=useState(0)
-    const[users,setUsers]=useState(['Alex','Dimych','Bob'])
+export const Example = () => {
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Alex', 'Dimych', 'Bob'])
+
+    const newArray = useMemo(() => {
+        const newArray = users.filter(u => u.toLowerCase().indexOf('a') > -1)
+        return newArray
+    }, [users])
+    const addUsers = () => {
+        const newUsers = [...users, 'Sveta' + new Date().getTime()]
+        setUsers(newUsers)
+    }
+
 
     return <>
-    <button onClick={()=>setCounter(counter+1)}>+</button>
-    <NewMessageCounter count={counter}/>
-    <Users users={users}/>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={() => addUsers()}>add users</button>
+        <NewMessageCounter count={counter}/>
+        <Users users={users}/>
     </>
+}
+
+export const Difficult=()=>{
+    const[a,setA]=useState<number>(5)
+    const[b,setB]=useState<number>(5)
+
+    let resultA=1
+    let resultB=1
+
+
+    resultA=useMemo(()=>{
+        let tempResultA=1
+        for(let i=1;i<=a;i++){
+            let fake=0
+            while (fake<100000000){
+                fake++
+                const fakeValue=Math.random()
+            }
+            tempResultA=tempResultA*i
+        }
+        return tempResultA
+    },[a])
+    for(let i=1;i<=b;i++){
+        resultB*=i
+    }
 }
